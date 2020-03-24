3b53fc88412aefd8ee2c3160057fe28d22082802
faudel.hadroug@epitech.eu <faudel.hadroug@epitech.eu@epitech.eu>
2020-03-24 17:45:50 +0100

    ajout dimage, reset css, jquery, htaccess, robots.txt. modification /index.php - fin de letape 1

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+echo '<pre>'.print_r($_POST).'</pre>';
+echo '<pre>'.print_r($_GET).'</pre>';
+echo '<pre>'.print_r($_SERVER).'</pre>';
 
 define('BASE_URI', str_replace('\\', '/', substr (__DIR__, strlen($_SERVER['DOCUMENT_ROOT']))));
 require_once(implode(DIRECTORY_SEPARATOR, ['Core', 'autoload.php']));
 
 $app = new Core\Core();
 $app->run();
-
--- src/View/User/login.php
+++ src/View/User/login.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div id="div-login-form">
-        <form action="../User/index.php" id="login-form">
+        <form action="/github/MVC_PiePHP/index.php" id="login-form">   <!-- action="../User/index.php"  -->
             <input type="email" name='email-form' id="email-form">
             <input type="password" name="password-form" id="password-form">
             <input type="submit" value="Validé">
