812c81bed382111588992a3704ce11fdc16381ac
faudel.hadroug@epitech.eu <faudel.hadroug@epitech.eu@epitech.eu>
2020-03-24 08:46:23 +0100

    introduction finis, prêt pour commencer "lancement de lapplication"

---
+++ src/View/User/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo '<pre>'.print_r($_POST).'</pre>';
+echo '<pre>'.print_r($_GET).'</pre>';
+echo '<pre>'.print_r($_SERVER).'</pre>';
---
+++ src/View/User/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="div-login-form">
+        <form action="../User/index.php" id="login-form">
+            <input type="email" name='email-form' id="email-form">
+            <input type="password" name="password-form" id="password-form">
+            <input type="submit" value="Validé">
+        </form>
+    </div>
+</body>
+</html>
